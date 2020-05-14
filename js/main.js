5326daa47494a4fb6848f0e01eff7adf0eb8e63a
KanCs <Kan@DESKTOP-TE49KPA>
2020-05-14 17:25:35 -0500

    Funcionalidad de formulario terminada

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
-function enviar() {
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 4,
+    loop: true,
+    autoWidth: true,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function() {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function() {
+    owl.trigger('stop.owl.autoplay')
+})
+$('#Reservar').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 'slow');
+    return false;
+});
+$('#GuardarNuevaCita').click(function() {
     nombre = $('#nombre').val();
     correo = $('#correo').val();
     telefono = $('#telefono').val();
     Reservar(nombre, correo, telefono);
-    $("#GuardarNuevaCita").click();
-}
-$('#GuardarNuevaCita').click(function() {
-
 });
 
 function Reservar(nombre, correo, telefono) {
@@ -21,11 +39,15 @@ function Reservar(nombre, correo, telefono) {
         data: cadena,
         success: function(r) {
             if (r == 1) {
-                alertify.success("Agregado con exito");
+                document.getElementById('nombre').value = "";
+                document.getElementById('correo').value = "";
+                document.getElementById('telefono').value = "";
+                alertify.success("¡Cita creada exitosamente!");
+
             } else if (r == 2) {
                 alertify.error("Complete todos los campos");
             } else if (r == 3) {
-                alertify.error("El correo ya se encuentra registrado");
+                alertify.error("¡Ya tiene una cita programada!");
             } else {
                 alertify.error("Error");
             }
